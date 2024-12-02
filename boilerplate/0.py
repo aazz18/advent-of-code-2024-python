a6027981529d54c9fbbd11d4fdd94f415393d4e7
aazz18 <andyzeng207@gmail.com>
2024-12-02 20:50:46 +0000

    Add boilerplate code for future use (easy)

---
+++ boilerplate/0.py
@@ -0,0 +1,19 @@
+import math
+
+def solution1(input: str) -> solution:int:
+    inputL = input.splitlines()
+    solution = 0
+    return solution
+
+def solution2(input: str) -> int: #  
+    inputL = input.splitlines()
+    solution = 0
+    return solution
+
+with open("01.txt", "r") as file:
+    input_data = file.read()
+
+solution1_ans = solution1(input_data)
+print(solution1_ans)
+solution2_ans = solution2(input_data)
+print(solution2_ans)
